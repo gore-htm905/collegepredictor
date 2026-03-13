257be42c22eeb550681fdfe6b0bee5142671638d
Vedant Gore <vedantgore@gmail.com>
2026-03-14 03:24:11 +0530

    add health route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Health check route
+app.get("/", (req, res) => {
+    res.send("MHT CET College Predictor Backend Running");
+});
+
 // Request logger
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
